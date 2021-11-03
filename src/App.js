3823404b22f38d871653118ab3fbef02ef5f7fde
Decario <edwyntoure@yahoo.com>
2021-11-03 12:34:01 +0100

    change title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn hard React
+          Learn React
         </a>
       </header>
     </div>
